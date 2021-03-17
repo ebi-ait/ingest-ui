3b8b1e65aaf8eb5b0fb0f9f351447eb528081682
Jacob Windsor <me@jcbwndsr.com>
2021-03-17 17:04:16 +0100

    <204> Fix lint error

--- client/src/app/metadata-schema-form/metadata-form/metadata-form.component.ts
+++ client/src/app/metadata-schema-form/metadata-form/metadata-form.component.ts
@@ -22,7 +22,7 @@ export class MetadataFormComponent implements OnInit {
 
   @Input() selectedTabKey: string;
 
-  @Input() useVf: boolean = false;
+  @Input() useVf = false;
 
   @Input() data: object;
 
