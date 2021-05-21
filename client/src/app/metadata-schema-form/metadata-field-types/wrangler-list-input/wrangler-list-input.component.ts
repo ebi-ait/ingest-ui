6b184667a176f13e035c9e2b37e0a9e802197be4
Alegria Aclan <aaclan@ebi.ac.uk>
2021-05-21 17:06:04 +0100

    Store account id instead of account name in the primary wrangler field

--- client/src/app/metadata-schema-form/metadata-field-types/wrangler-list-input/wrangler-list-input.component.ts
+++ client/src/app/metadata-schema-form/metadata-field-types/wrangler-list-input/wrangler-list-input.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {BaseInputComponent} from '../base-input/base-input.component';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, map, startWith, switchMap} from 'rxjs/operators';
 import {IngestService} from '../../../shared/services/ingest.service';
 import {AaiService} from '../../../aai/aai.service';
 import {Metadata} from '../../models/metadata';
 import {FormControl} from '@angular/forms';
+import {Account} from '../../../core/account';
+import {AlertService} from '../../../shared/services/alert.service';
 
 @Component({
   selector: 'app-wrangler-list-input',
@@ -17,10 +19,16 @@ export class WranglerListInputComponent extends BaseInputComponent implements On
   @Input() control: FormControl;
   @Input() id: string;
 
-  options: string[];
-  defaultOption: string;
+  searchControl: FormControl;
+  options$: Observable<Account[]>;
+  defaultOption: Account;
 
-  constructor(private ingestService: IngestService, private aaiService: AaiService) {
+  wranglers$: Observable<Account[]>;
+  selectedWrangler: Account;
+
+  constructor(private ingestService: IngestService,
+              private aaiService: AaiService,
+              private alertService: AlertService) {
     super();
     super.metadata = this.metadata;
     super.control = this.control;
@@ -29,12 +37,38 @@ export class WranglerListInputComponent extends BaseInputComponent implements On
 
   ngOnInit(): void {
     super.ngOnInit();
-    this.populateWranglerList();
     this.setDefaultWrangler();
+    this.searchControl = this.createSearchControl(this.control.value);
+    this.wranglers$ = this.ingestService.getWranglers();
+    this.options$ = this.searchControl.valueChanges
+      .pipe(
+        startWith(this.searchControl.value ? this.searchControl.value : ''),
+        filter(text => text.length === 0 || text.length > 2),
+        debounceTime(10),
+        distinctUntilChanged(),
+        switchMap(newSearch => this.onSearchValueChanged(newSearch))
+      );
+    this._filter(this.control.value).subscribe(
+      wranglers => {
+        if (wranglers.length === 1) {
+          this.selectedWrangler = wranglers[0];
+        } else {
+          this.alertService.error('Error', 'The wrangler for this project could not be identified.' +
+            ' Please email hca-ingest-dev@ebi.ac.uk .');
+        }
+      }
+    );
   }
 
-  private populateWranglerList() {
-    this.getWranglerNames().subscribe((wranglerList) => this.options = wranglerList);
+  createSearchControl(value: string) {
+    return new FormControl({
+      value: value,
+      disabled: this.metadata.isDisabled
+    });
+  }
+
+  displayWranglerName(wrangler: Account) {
+    return wrangler ? wrangler.name : '';
   }
 
   private getWranglerNames(): Observable<string[]> {
@@ -43,24 +77,26 @@ export class WranglerListInputComponent extends BaseInputComponent implements On
   }
 
   private setDefaultWrangler() {
-    this.aaiService.getUser().pipe(map(user => user?.profile?.name)).
-      subscribe(name => {
-        this.defaultOption = name;
-        if (!this.control.value) {
-          this.control.setValue(name);
-        }
-      });
-
+    // TODO set current account as default wrangler
   }
 
-  onValueChange(value: string) {
-    this.control.markAllAsTouched();
-    if (!value) {
-      this.control.reset();
-    } else {
-      this.control.setValue(value);
-    }
+  private onSearchValueChanged(value: string | Account): Observable<Account[]> {
+    const searchText = typeof value === 'string' ? value.toLowerCase() : value.name ? value.name.toLowerCase() : '';
+    return this._filter(searchText);
+  }
+
+  private _filter(name_or_id: string): Observable<Account[]> {
+    const filterValue = name_or_id.toLowerCase();
+    return this.wranglers$.pipe(
+      map(wranglers => wranglers.filter(wrangler => {
+          return wrangler.name.toLowerCase().indexOf(filterValue) === 0 || wrangler.id === name_or_id;
+        })
+      )
+    );
+  }
+
+  updateControl(value: Account) {
+    this.control.setValue(value.id);
+    this.selectedWrangler = value;
   }
 }
-
-
