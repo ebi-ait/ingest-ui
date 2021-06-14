97e6bf58e7deb021719293839619f9e9f95ea975
Karoly Erdos <karoly@ebi.ac.uk>
2021-06-14 16:10:41 +0100

    Revert URL check to the previous code as Regexp was too expensive and use mg-template

--- client/src/app/shared/components/data-table/data-table.component.ts
+++ client/src/app/shared/components/data-table/data-table.component.ts
@@ -66,12 +66,11 @@ export class DataTableComponent implements OnInit, OnDestroy {
   }
 
   isUrl(value: string) {
-    const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-    return !!pattern.test(value);
+    try {
+      const url = new URL(value);
+    } catch (_) {
+      return false;
+    }
+    return true;
   }
 }
