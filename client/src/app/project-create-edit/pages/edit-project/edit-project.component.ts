aaaf48ecb9edc9538eb1243776a9c64b7fedb4b8
Jacob Windsor <me@jcbwndsr.com>
2021-07-26 20:34:01 +0200

    Handle errors

--- client/src/app/project-create-edit/pages/edit-project/edit-project.component.ts
+++ client/src/app/project-create-edit/pages/edit-project/edit-project.component.ts
@@ -3,6 +3,7 @@ import {Observable} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {IngestService} from '../../../shared/services/ingest.service';
 import {AlertService} from '../../../shared/services/alert.service';
+import {catchError} from "rxjs/operators";
 
 @Component({
   selector: 'app-edit-project',
@@ -21,6 +22,9 @@ export class EditProjectComponent implements OnInit {
   ngOnInit() {
     const pathVariables = this.route.snapshot.paramMap;
     const projectUuid: string = pathVariables.get('uuid');
-    this.project$ = this.ingestService.getProjectByUuid(projectUuid); // TODO handle errors
+    this.project$ = this.ingestService.getProjectByUuid(projectUuid);
+    this.project$.subscribe(() => {}, err => {
+      this.alertService.error('Project could not be retrieved.', err.message);
+    });
   }
 }
