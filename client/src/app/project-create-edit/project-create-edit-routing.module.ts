97b4ca8b32d8791731c620953c15d4e9a9bcd897
Jacob Windsor <me@jcbwndsr.com>
2021-07-27 15:23:36 +0200

    Add wrangler or owner guard

--- client/src/app/project-create-edit/project-create-edit-routing.module.ts
+++ client/src/app/project-create-edit/project-create-edit-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {AutofillProjectFormComponent} from './pages/autofill-project-form/autofill-project-form.component';
 import {EditProjectComponent} from './pages/edit-project/edit-project.component';
 import {CreateProjectComponent} from './pages/create-project/create-project.component';
+import {WranglerOrOwnerGuard} from '../shared/guards/wrangler-or-owner.guard';
 
 const routes: Routes = [
   { path: 'register/autofill', component: AutofillProjectFormComponent },
   { path: 'register', component: CreateProjectComponent },
-  { path: ':uuid/edit', component: EditProjectComponent },
+  { path: ':uuid/edit', component: EditProjectComponent, canActivate: [ WranglerOrOwnerGuard ] },
 ];
 
 @NgModule({
