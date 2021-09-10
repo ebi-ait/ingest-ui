34aeeefb58301fb0be9328fd6a10b8d5dd974884
Alegria Aclan <aaclan@ebi.ac.uk>
2021-09-10 17:27:49 +0100

    Display both description and guideline as helper text

--- client/src/app/metadata-schema-form/metadata-field-types/base-input/base-input.component.ts
+++ client/src/app/metadata-schema-form/metadata-field-types/base-input/base-input.component.ts
@@ -33,11 +33,13 @@ export class BaseInputComponent implements OnInit {
   ngOnInit(): void {
     const userFriendly = this.metadata.schema.user_friendly;
     const title = this.metadata.schema.title;
+
+    const guidelines = this.metadata.schema.guidelines;
     const description = this.metadata.schema.description;
 
     // TODO consolidate form item data
     this.label = userFriendly ? userFriendly : title ? title : this.metadata.key;
-    this.helperText = description;
+    this.helperText = (description || '') + (guidelines || '');
     this.isRequired = this.metadata.isRequired;
     this.disabled = this.metadata.isDisabled || this.metadata.isDisabled;
     this.data = <FormItemData> {
