327ea51a6df5a1221d6a1d0a578f723b5cd3c720
Alegria Aclan <aaclan@ebi.ac.uk>
2021-09-10 17:09:49 +0100

    Use schema as description as helper text

--- client/src/app/metadata-schema-form/metadata-field-types/base-input/base-input.component.ts
+++ client/src/app/metadata-schema-form/metadata-field-types/base-input/base-input.component.ts
@@ -33,14 +33,11 @@ export class BaseInputComponent implements OnInit {
   ngOnInit(): void {
     const userFriendly = this.metadata.schema.user_friendly;
     const title = this.metadata.schema.title;
-
-
-    const guidelines = this.metadata.schema.guidelines;
     const description = this.metadata.schema.description;
 
     // TODO consolidate form item data
     this.label = userFriendly ? userFriendly : title ? title : this.metadata.key;
-    this.helperText = guidelines ? guidelines : description;
+    this.helperText = description;
     this.isRequired = this.metadata.isRequired;
     this.disabled = this.metadata.isDisabled || this.metadata.isDisabled;
     this.data = <FormItemData> {
