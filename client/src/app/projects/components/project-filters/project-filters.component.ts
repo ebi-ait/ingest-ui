a467a90fc91e88c5e063030f6f00d168951157ff
Jacob Windsor <me@jcbwndsr.com>
2021-09-30 16:38:08 +0100

    Allow for disabling filtering by cell count
    The control form elements (those not outputted by the component) have been put into a form sub-group

--- client/src/app/projects/components/project-filters/project-filters.component.ts
+++ client/src/app/projects/components/project-filters/project-filters.component.ts
@@ -1,8 +1,9 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {isNil} from 'lodash';
 import {Observable} from 'rxjs';
-import {debounceTime, map} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, map, switchMap, tap} from 'rxjs/operators';
 import {Account} from '../../../core/account';
 import {IngestService} from '../../../shared/services/ingest.service';
 import {OntologyService} from '../../../shared/services/ontology.service';
@@ -26,21 +27,22 @@ export class ProjectFiltersComponent implements OnInit {
     primaryWrangler: [],
     wranglingPriority: [],
     hasOfficialHcaPublication: [],
-    minCellCount: [0],
-    maxCellCount: [this.maxCellCount],
+    minCellCount: [{value: 0, disabled: true}],
+    maxCellCount: [{value: this.maxCellCount, disabled: true}],
     identifyingOrganism: [],
-    organOntology: []
+    organOntology: [],
+
+    // UI controls that are not part of the output of this component
+    controlsForm: this.fb.group({
+      filterByCellCount: [false],
+      organSearchValue: ['']
+    })
   });
+
   wranglers$: Observable<Account[]>;
-  organs: any[];
+  organs$: Observable<any[]>;
   wranglingStates = ingestSchema['properties']['wranglingState']['enum'];
   @Output() filters: EventEmitter<ProjectFilters> = new EventEmitter(this.filtersForm.value);
-
-  // The organ search input is not part of the formGroup (see ngModelOptions value in component)
-  // since we don't want the search value in the form output, just the value of the selected organ ontology
-  // This allows for the search value to be displayed with the full organ name and send just the ontology behind the
-  // scenes
-  organSearchValue = '';
 
   isExpanded = false;
 
@@ -55,16 +57,47 @@ export class ProjectFiltersComponent implements OnInit {
 
   ngOnInit(): void {
     this.wranglers$ = this.ingestService.getWranglers();
+
+    this.organs$ = this.filtersForm.get('controlsForm').valueChanges.pipe(
+      debounceTime(100),
+      tap(({ filterByCellCount }) => this.toggleFilterByCellCount(filterByCellCount)),
+      tap(({ organSearchValue }) => {
+        if (!organSearchValue) {
+          // Clear ontology value if search value is cleared
+          this.filtersForm.patchValue({organOntology: ''});
+        }
+      }),
+      switchMap(({ organSearchValue }) => this.fetchOrgans(organSearchValue))
+    );
+
     this.filtersForm.valueChanges.pipe(
+      distinctUntilChanged(),
       debounceTime(200),
       map(values => {
-        values['maxCellCount'] = this.getRawCellCount(values['maxCellCount']);
-        values['minCellCount'] = this.getRawCellCount(values['minCellCount']);
+        ['maxCellCount', 'minCellCount'].forEach(value => {
+          if (!isNil(values[value])) {
+            values[value] = this.getRawCellCount(values[value]);
+          }
+        });
+        // Remove values that are not part of ProjectFilters
+        delete values.controlsForm;
         return values;
       })
     ).subscribe(value => {
       this.filters.emit(value);
     });
+  }
+
+  toggleFilterByCellCount(enabled = false) {
+    ['maxCellCount', 'minCellCount'].forEach(control => this.filtersForm.controls[control][enabled ? 'enable' : 'disable']());
+  }
+
+  fetchOrgans(organSearchValue: string): Observable<any> {
+    return this.ols.lookup(
+      // @ts-ignore
+      ingestSchema['properties']['organ']['properties']['ontologies']['items'],
+      organSearchValue.toLowerCase()
+    );
   }
 
   transformWranglingState(wranglingState: String) {
@@ -89,29 +122,10 @@ export class ProjectFiltersComponent implements OnInit {
     this.filtersForm.patchValue({
       organOntology: organ.ontology
     });
-    this.organSearchValue = this.formatOrgan(organ);
+    this.filtersForm.get('controlsForm').patchValue({organSearchValue: this.formatOrgan(organ)});
   }
 
-  organSearchChange(value: string) {
-    if (!value) {
-      this.filtersForm.patchValue({
-        organOntology: null
-      });
-    }
-
-    if (typeof value !== 'string') {
-      // value could be an object since this will also be called by the autocomplete dropdown when an option is selected
-      // Ignore if not a string
-      return;
-    }
-    // @ts-ignore
-    this.ols.lookup(ingestSchema['properties']['organ']['properties']['ontologies']['items'], value.toLowerCase())
-      .subscribe(organs => {
-        this.organs = organs;
-      });
-  }
-
-  formatLabel = (value: number) => {
+  formatCellCountLabel = (value: number) => {
     const rawValue = this.getRawCellCount(value);
     const ONE_MILLION = 10 ** 6;
     if (rawValue >= ONE_MILLION) {
