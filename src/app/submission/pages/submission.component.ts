f6ca05b6a66fa79a30d43c98bd0a4c5b7287ed8b
Jacob Windsor <me@jcbwndsr.com>
2021-12-16 12:22:15 +0000

    Disable button instead of pre-emptively setting state
    Avoids a strange "flash" between states that can happen if the polling is a bit slow

--- src/app/submission/pages/submission.component.ts
+++ src/app/submission/pages/submission.component.ts
@@ -25,6 +25,8 @@ enum SubmissionTab {
   FILES = 3
 }
 
+const SUBMISSION_POLL_INTERVAL = 5000;
+
 @Component({
   selector: 'app-submission',
   templateUrl: './submission.component.html',
@@ -54,6 +56,7 @@ export class SubmissionComponent implements OnInit, OnDestroy {
   selectedIndex: any = 0;
   validationSummary: SubmissionSummary;
   isLoading: boolean;
+  graphValidationButtonDisabled = false;
 
   submissionDataSource: SimpleDataSource<SubmissionEnvelope>;
   projectDataSource: SimpleDataSource<Project>;
@@ -100,7 +103,7 @@ export class SubmissionComponent implements OnInit, OnDestroy {
 
   connectSubmissionEnvelope() {
     this.submissionDataSource = new SimpleDataSource<SubmissionEnvelope>(this.submissionEnvelopeEndpoint.bind(this));
-    this.submissionDataSource.connect(true).subscribe(submissionEnvelope => {
+    this.submissionDataSource.connect(true, SUBMISSION_POLL_INTERVAL).subscribe(submissionEnvelope => {
       this.initSubmissionAttributes(submissionEnvelope);
       this.displaySubmissionErrors(submissionEnvelope);
       this.checkFromManifestIfLinkingIsDone(submissionEnvelope);
@@ -414,11 +417,12 @@ export class SubmissionComponent implements OnInit, OnDestroy {
     const url = `${this.submissionEnvelope['_links']['self']['href']}/graphValidationRequestedEvent`
     this.ingestService.put(url, {}).subscribe(
       (submissionEnvelope) => {
-        // Pre-emptively set the validation state
-        this.submissionState = SUBMISSION_STATES.GraphValidationRequested;
+        this.graphValidationButtonDisabled = true;
+        setTimeout(() => this.graphValidationButtonDisabled = false, SUBMISSION_POLL_INTERVAL * 4/3)
       },
       err => {
-        this.alertService.error('An error occurred while triggering validation', err.message)
+        this.alertService.error('An error occurred while triggering validation', err.message);
+        this.graphValidationButtonDisabled = false;
       }
     )
   }
