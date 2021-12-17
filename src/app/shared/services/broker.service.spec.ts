8de40191a6fcfd9dcedb8516cd29359ec280d90f
Alegria Aclan <aaclan@ebi.ac.uk>
2021-12-17 12:37:55 +0000

    Simulate delay and check timeout error

--- src/app/shared/services/broker.service.spec.ts
+++ src/app/shared/services/broker.service.spec.ts
@@ -1,6 +1,6 @@
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {TestBed} from '@angular/core/testing';
+import {fakeAsync, flush, TestBed, tick} from '@angular/core/testing';
 import {BrokerService} from './broker.service';
 import {TimeoutError} from 'rxjs';
 
@@ -20,6 +20,7 @@ describe('Broker Service', () => {
     httpClient = TestBed.inject(HttpClient);
     httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(BrokerService);
+
   });
 
   afterEach(() => {
@@ -34,41 +35,37 @@ describe('Broker Service', () => {
     const submissionUuid = 'sub_uuid'
 
     it(`should get response`, (done) => {
-
       const blob = new Blob([],
         {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
-        service.downloadSpreadsheet(submissionUuid)
-            .subscribe(res => {
-              expect(res.data).toEqual(blob);
-              done();
-            });
-        const req = httpTestingController.expectOne(`${api_url}/submissions/${submissionUuid}/spreadsheet`);
-
-        req.flush(blob, {
-          headers: { 'Content-Disposition': 'attachment; filename=myfile.xls' },
-          status: 200,
-          statusText: 'OK'
+      service.downloadSpreadsheet(submissionUuid)
+        .subscribe(res => {
+          expect(res.data).toEqual(blob);
+          done();
         });
+      const req = httpTestingController.expectOne(`${api_url}/submissions/${submissionUuid}/spreadsheet`);
+
+      req.flush(blob, {
+        headers: {'Content-Disposition': 'attachment; filename=myfile.xls'},
+        status: 200,
+        statusText: 'OK'
+      });
 
       expect(req.request.method).toEqual('GET');
       expect(req.request.body).toBeNull();
-      
+
     });
 
-    it(`should timeout`, (done) => {
+    it(`should timeout`, fakeAsync(() => {
       service.downloadSpreadsheet(submissionUuid)
-      .subscribe(res => {},
-        err => {
-          expect(err.statusText).toEqual('Request Timeout');
-          done();
-        });
+        .subscribe(res => {
+          },
+          err => {
+            expect(err instanceof TimeoutError).toBeTruthy();
+          });
 
-      let req = httpTestingController.expectOne(`${api_url}/submissions/${submissionUuid}/spreadsheet`);
-      req.flush(null, {
-        status: 408,
-        statusText: 'Request Timeout'
-      });
-    });
+      httpTestingController.expectOne(`${api_url}/submissions/${submissionUuid}/spreadsheet`);
+      tick(service.DOWNLOAD_SPREADSHEET_TIMEOUT);
+    }));
 
   });
 });
