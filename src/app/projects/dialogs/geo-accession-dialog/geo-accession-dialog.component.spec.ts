5f4f55b2fd5a476a12f740abc2f0a87e3367a74b
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-01-24 11:57:39 +0000

    Create Jasmine spies of dependencies.

--- src/app/projects/dialogs/geo-accession-dialog/geo-accession-dialog.component.spec.ts
+++ src/app/projects/dialogs/geo-accession-dialog/geo-accession-dialog.component.spec.ts
@@ -1,25 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { GeoAccessionDialogComponent } from './geo-accession-dialog.component';
+import {GeoAccessionDialogComponent} from './geo-accession-dialog.component';
+import {MatDialogRef} from '@angular/material/dialog';
+import {BrokerService} from '@shared/services/broker.service';
+import {LoaderService} from '@shared/services/loader.service';
+import {AlertService} from '@shared/services/alert.service';
+import {SaveFileService} from '@shared/services/save-file.service';
+import SpyObj = jasmine.SpyObj;
 
 describe('GeoAccessionDialogComponent', () => {
   let component: GeoAccessionDialogComponent;
-  let fixture: ComponentFixture<GeoAccessionDialogComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GeoAccessionDialogComponent ]
-    })
-    .compileComponents();
-  });
+  let dialogRef: SpyObj<MatDialogRef<GeoAccessionDialogComponent>>;
+  let brokerSvc: SpyObj<BrokerService>;
+  let loaderSvc: SpyObj<LoaderService>;
+  let alertSvc: SpyObj<AlertService>;
+  let saveFileSvc: SpyObj<SaveFileService>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GeoAccessionDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+    brokerSvc = jasmine.createSpyObj('BrokerService', ['downloadSpreadsheet']);
+    loaderSvc = jasmine.createSpyObj('LoaderService', ['display']);
+    alertSvc = jasmine.createSpyObj('AlertService', ['clear', 'error']);
+    saveFileSvc = jasmine.createSpyObj('SaveFileService', ['saveFile']);
+
+    component = new GeoAccessionDialogComponent(dialogRef, brokerSvc, loaderSvc, alertSvc, saveFileSvc);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should test onDownload method', () => {
+    throw new Error('Not Implemented');
+  });
+
+  it('should test onClose method', () => {
+    throw new Error('Not Implemented');
   });
 });
