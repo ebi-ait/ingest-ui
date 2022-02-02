021957f5363fa316fc88d13b22e4e875e4af2875
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-02-02 11:49:56 +0000

    Refactor unit tests to prepare for more

--- src/app/metadata-schema-form/metadata-form/metadata-form.component.spec.ts
+++ src/app/metadata-schema-form/metadata-form/metadata-form.component.spec.ts
@@ -1,104 +1,106 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {MetadataFormService} from '../metadata-form.service';
 import {JsonSchema} from '../models/json-schema';
 import {MetadataForm} from '../models/metadata-form';
 import {MetadataFormConfig} from '../models/metadata-form-config';
 import {MetadataFormTab} from '../models/metadata-form-layout';
 import {MetadataFormComponent} from './metadata-form.component';
-import SpyObj = jasmine.SpyObj;
-
-function getSchema() {
-  return {
-    'name': 'project'
-  } as JsonSchema;
-}
-
-function getData() {
-  return {
-    content: null,
-    admin: {
-      key: 'value'
-    }
-  };
-}
-
-function createTab(title) {
-  return {
-    title: title,
-    key: 'project.' + title,
-    items: [
-      'project.' + title
-    ]
-  } as MetadataFormTab;
-}
-
-function createMetadataFormConfig(tabs: MetadataFormTab[]) {
-  return {
-    viewMode: true,
-    hideEmptyFields: true,
-    layout: {
-      tabs: tabs
-    }
-  } as MetadataFormConfig;
-}
-
-function createFormGroup() {
-  return new FormGroup({
-    x: new FormControl('', Validators.required),
-    y: new FormControl('', Validators.required),
-    z: new FormControl('', Validators.required),
-  });
-}
-
-function createMetadataForm(tabs: MetadataFormTab[]): MetadataForm {
-  return {
-    key: 'test key',
-    jsonSchema: getSchema(),
-    config: createMetadataFormConfig(tabs),
-    metadataRegistry: jasmine.createSpyObj('MetadataRegistry', ['buildMetadataRegistry']),
-    formGroup: createFormGroup()
-  } as MetadataForm;
-}
 
 describe('MetadataFormComponent', () => {
   let component: MetadataFormComponent;
   let fixture: ComponentFixture<MetadataFormComponent>;
 
-  let metadataFormSvc: SpyObj<MetadataFormService>;
+  const metadataFormSvc = jasmine.createSpyObj('MetadataFormService', ['createForm', 'cleanFormData']);
+  const metadataRegistrySpy = jasmine.createSpyObj('MetadataRegistry', ['buildMetadataRegistry']);
+  const formGroupSpy = jasmine.createSpyObj('FormGroup', ['getRawValue', 'valueChanges'], ['valid']);
 
-  const contentTab = createTab('content.contributors');
-  const adminTab = createTab('admin');
-  const tabs = [contentTab, adminTab];
+  interface metadataFormDependencies {
+    key?: string;
+    schema?: JsonSchema;
+    config?: MetadataFormConfig;
+  }
 
-  beforeEach(async(() => {
-    metadataFormSvc = jasmine.createSpyObj('MetadataFormService', ['createForm']);
-    metadataFormSvc.createForm.and.returnValue(createMetadataForm(tabs));
-    TestBed.configureTestingModule({
-      providers: [
-        { provide: MetadataFormService, useValue: metadataFormSvc},
-      ],
-      declarations: [MetadataFormComponent]
-    })
-      .compileComponents();
-  }));
+  function createTab(title) {
+    return {
+      title: title,
+      key: 'project.' + title,
+      items: [
+        'project.' + title
+      ]
+    } as MetadataFormTab;
+  }
+
+  function configureFormAndSvc({
+                                 key = 'test key',
+                                 schema = {'name': 'project'} as JsonSchema,
+                                 config = {layout: {tabs: [createTab('tab')]}}
+                               }: metadataFormDependencies) {
+    const form = {
+      key: key,
+      jsonSchema: schema as JsonSchema,
+      config: config,
+      metadataRegistry: metadataRegistrySpy,
+      formGroup: formGroupSpy
+    } as MetadataForm;
+    metadataFormSvc.createForm.and.returnValue(form);
+    component.form = form;
+    component.formGroup = formGroupSpy;
+    component.schema = schema;
+    component.config = config;
+  }
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: MetadataFormService, useValue: metadataFormSvc},
+      ],
+      declarations: [MetadataFormComponent]
+    }).compileComponents();
     fixture = TestBed.createComponent(MetadataFormComponent);
     component = fixture.componentInstance;
-    component.schema = getSchema();
-    component.data = getData();
-    component.config = createMetadataFormConfig(tabs);
-    component.selectedTabKey = 'content';
-    component.visibleTabs = tabs;
-    fixture.detectChanges();
+    spyOn(component, 'onFormChange');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('tabIsVisible should pass with null content', () => {
-    expect(component.tabIsVisible(contentTab)).toBe(false);
+  describe('tab tests', () => {
+    const contentTab = createTab('content.contributors');
+    const adminTab = createTab('admin');
+    const tabs = [contentTab, adminTab];
+
+    const config = {
+      viewMode: true,
+      hideEmptyFields: true,
+      layout: {
+        tabs: tabs
+      }
+    } as MetadataFormConfig;
+
+    const schema = {
+      'name': 'project'
+    } as JsonSchema;
+
+    const data = {
+      content: null,
+      admin: {
+        key: 'value'
+      }
+    };
+
+    beforeEach(() => {
+      configureFormAndSvc({schema: schema, config: config});
+      component.schema = schema;
+      component.data = data;
+      component.config = config;
+      component.selectedTabKey = 'content';
+      component.visibleTabs = tabs;
+      fixture.detectChanges();
+    });
+
+    it('tabIsVisible should pass with null content', () => {
+      expect(component.tabIsVisible(contentTab)).toBe(false);
+    });
   });
 });
