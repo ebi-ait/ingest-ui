d87ddfe35dbc4afc53ad4e8cffb8db0062bf6e47
Alegria Aclan <aaclan@ebi.ac.uk>
2022-02-18 18:00:00 +0000

    dcp-673 Optimise and sort imports

--- src/app/submission/submission.module.ts
+++ src/app/submission/submission.module.ts
@@ -1,5 +1,5 @@
-import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
@@ -7,22 +7,22 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatMenuModule} from "@angular/material/menu";
 import {MatTabsModule} from "@angular/material/tabs";
 import {MaterialModule} from "@app/material.module";
-import {UploadInfoComponent} from "./components/upload-info/upload-info.component";
-import {MetadataPickerComponent} from "./components/metadata-picker/metadata-picker.component";
-import {NgxGraphModule} from "@swimlane/ngx-graph";
-import {SubmissionRoutingModule} from "./submission-routing.module";
-import {ProcessDetailsComponent} from "./components/process-details/process-details.component";
+import {MetadataSchemaFormModule} from "@metadata-schema-form/metadata-schema-form.module";
+import {SharedModule} from "@shared/shared.module";
 import {NgxDatatableModule} from "@swimlane/ngx-datatable";
+import {NgxGraphModule} from "@swimlane/ngx-graph";
 import {EntityValidationSummaryComponent} from "./components/entity-validation-summary/entity-validation-summary.component";
 import {FilesComponent} from "./components/files/files.component";
 import {MetadataListComponent} from "./components/metadata-list/metadata-list.component";
+import {MetadataPickerComponent} from "./components/metadata-picker/metadata-picker.component";
+import {ProcessDetailsComponent} from "./components/process-details/process-details.component";
+import {ProcessNodeDetailsComponent} from './components/process-node-details/process-node-details.component';
+import {SpreadsheetTabDetailsComponent} from './components/spreadsheet-tab-details/spreadsheet-tab-details.component';
 import {SubmitComponent} from "./components/submit/submit.component";
+import {UploadInfoComponent} from "./components/upload-info/upload-info.component";
 import {ValidationSummaryComponent} from "./components/validation-summary/validation-summary.component";
 import {SubmissionComponent} from "./pages/submission.component";
-import {SharedModule} from "@shared/shared.module";
-import {MetadataSchemaFormModule} from "@metadata-schema-form/metadata-schema-form.module";
-import { SpreadsheetTabDetailsComponent } from './components/spreadsheet-tab-details/spreadsheet-tab-details.component';
-import { ProcessNodeDetailsComponent } from './components/process-node-details/process-node-details.component';
+import {SubmissionRoutingModule} from "./submission-routing.module";
 
 @NgModule({
   declarations: [
