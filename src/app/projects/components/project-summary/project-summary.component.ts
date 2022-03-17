b4b1990e2a8a95f2e7afb6825e413b0c3a318a27
Jacob Windsor <me@jcbwndsr.com>
2022-03-17 14:46:25 +0100

    dcp-707 fix imports

--- src/app/projects/components/project-summary/project-summary.component.ts
+++ src/app/projects/components/project-summary/project-summary.component.ts
@@ -1,14 +1,14 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Account} from "@app/core/account";
 import {MetadataFormConfig} from '@metadata-schema-form/models/metadata-form-config';
+import getLayout from '@projects/components/project-metadata-form/layout';
+import * as ingestSchema from '@projects/schemas/project-ingest-schema.json';
 import {SUBMISSION_STATES} from '@shared/constants';
 import {Project} from '@shared/models/project';
 import {AlertService} from '@shared/services/alert.service';
 import {IngestService} from '@shared/services/ingest.service';
 import {SchemaService} from '@shared/services/schema.service';
 import {Observable} from 'rxjs';
-import {Account} from '../core/account';
-import getLayout from '../projects/components/project-metadata-form/layout';
-import * as ingestSchema from '../projects/schemas/project-ingest-schema.json';
 
 
 @Component({
