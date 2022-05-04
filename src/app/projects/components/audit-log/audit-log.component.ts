f499e93a12871f6630de5949c6a799d2c49e024c
Yusra Haider <yhaider@ebi.ac.uk>
2022-05-05 00:08:15 +0100

    ebi-ait/dcp-ingest-central#547: formatted event column

--- src/app/projects/components/audit-log/audit-log.component.ts
+++ src/app/projects/components/audit-log/audit-log.component.ts
@@ -1,6 +1,7 @@
 import {formatDate} from "@angular/common";
 import {Component, Input} from '@angular/core';
 import {AuditLog} from "@projects/models/audit-log";
+import { lowerCase, startCase } from "lodash";
 
 @Component({
   selector: 'app-audit-log',
@@ -12,8 +13,16 @@ export class AuditLogComponent {
   @Input()
   auditLogs: AuditLog[];
 
+  getAuditEvent(auditLog: AuditLog) {
+    return `${auditLog['auditType']} from ${this.formatAuditEvent(auditLog['before'])} to ${
+      this.formatAuditEvent(auditLog['after'])}`;
+  }
+
   formatAuditDate(date: string): string {
     return formatDate(date, 'medium', 'en-GB');
   }
 
+  formatAuditEvent(event: string): string {
+    return (event ? startCase(lowerCase(event)) : 'None');
+  }
 }
