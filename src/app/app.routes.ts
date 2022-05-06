9129fa3d1bdd69eec350d97b662333cab465ba2d
Jacob Windsor <me@jcbwndsr.com>
2022-05-06 11:43:14 +0200

    dcp-717 use markdown for guides

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import {Routes} from '@angular/router';
 import {AaiCallbackComponent} from './aai-callback/aai-callback.component';
-import {AllProjectsComponent} from './projects/pages/all-projects/all-projects.component';
 import {ErrorComponent} from './error/error.component';
 import {LoginComponent} from './login/login.component';
 import {MyProjectsComponent} from './my-projects/my-projects.component';
 import {RegistrationComponent} from './registration/registration.component';
 import {UserIsLoggedInGuard} from './shared/guards/user-is-logged-in.guard';
 import {UserIsWranglerGuard} from './shared/guards/user-is-wrangler.guard';
-import {WranglerOrOwnerGuard} from './shared/guards/wrangler-or-owner.guard';
 import {SubmissionListComponent} from './submission-list/submission-list.component';
 
 // eslint-disable-next-line max-len
@@ -34,6 +32,11 @@ export const ROUTES: Routes = [
     loadChildren: () =>
       import('./submission/submission.module').then((m) => m.SubmissionModule),
     canActivate: [UserIsLoggedInGuard]
+  },
+  {
+    path: 'guide',
+    loadChildren: () =>
+      import('./guide/guide.module').then((m) => m.GuideModule)
   },
 
   {path: 'submissions/list', component: SubmissionListComponent,  canActivate: [UserIsLoggedInGuard, UserIsWranglerGuard]},
