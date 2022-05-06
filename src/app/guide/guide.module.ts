97263138a841163e1746e6a5ece3f36ca0c9504a
Jacob Windsor <me@jcbwndsr.com>
2022-05-06 15:14:02 +0200

    dcp-717 add icon module

--- src/app/guide/guide.module.ts
+++ src/app/guide/guide.module.ts
@@ -1,6 +1,7 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {MatExpansionModule} from "@angular/material/expansion";
+import {MatIconModule} from "@angular/material/icon";
 import {GuideRoutingModule} from "@app/guide/guide-routing.module";
 import {MaterialModule} from "@app/material.module";
 import {DataSubmissionGuideComponent} from './pages/data-submission-guide/data-submission-guide.component';
@@ -14,7 +15,8 @@ import {DataSubmissionGuideComponent} from './pages/data-submission-guide/data-s
     CommonModule,
     GuideRoutingModule,
     MaterialModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatIconModule
   ]
 })
 export class GuideModule { }
