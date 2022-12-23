05d6a8b66c0f3af4295a8ea659041af348d938a9
Amnon Khen <amnon@ebi.ac.uk>
2022-12-23 10:34:32 +0000

    dcp-456 google analytics: trigger a pgaeview on route changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 import {IngestService} from '@shared/services/ingest.service';
 import {LoaderService} from '@shared/services/loader.service';
 import {Profile} from 'oidc-client';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {AaiService} from './aai/aai.service';
 import {Account} from './core/account';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -19,7 +22,10 @@ export class AppComponent implements OnInit {
   userProfile$: BehaviorSubject<Profile> = new BehaviorSubject<Profile>(undefined);
   userAccount$: Observable<Account>;
 
-  constructor(private loaderService: LoaderService, private aai: AaiService, private ingestService: IngestService) {
+  constructor(private loaderService: LoaderService,
+              private aai: AaiService,
+              private ingestService: IngestService,
+              public router: Router) {
     this.isSafari = window['safari'] !== undefined;
 
     this.loaderService.status.subscribe((val: boolean) => {
@@ -29,6 +35,11 @@ export class AppComponent implements OnInit {
     this.loaderService.message.subscribe((val: string) => {
       this.loaderMessage = val;
     });
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'UA-176603863-1', { 'page_path': event.urlAfterRedirects });
+      }
+    })
   }
   ngOnInit(): void {
     this.aai.getUserSubject().subject.subscribe(user => {
