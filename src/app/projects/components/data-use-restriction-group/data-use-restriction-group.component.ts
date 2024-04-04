92d14216cfad476b9cba6d60b5ec072c30934faa
Alexandros Orges Koci <koci.orges@hotmail.com>
2024-04-04 17:48:50 +0100

    dcp-999 DUO codes & DUOS id: Align DUOS ID validation logic with form control structure

--- src/app/projects/components/data-use-restriction-group/data-use-restriction-group.component.ts
+++ src/app/projects/components/data-use-restriction-group/data-use-restriction-group.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { MetadataForm } from '@metadata-schema-form/models/metadata-form';
 import { MetadataFormHelper } from '@metadata-schema-form/models/metadata-form-helper';
 import {first} from "rxjs/operators";
@@ -13,37 +13,32 @@ export class DataUseRestrictionGroupComponent implements OnInit {
   metadataForm: MetadataForm;
   form: FormGroup;
   formHelper: MetadataFormHelper;
-  duosIdCtrl: FormControl;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.formHelper = new MetadataFormHelper();
     this.initializeForms();
+    this.setDefaultDataUseRestrictionValue();
     this.subscribeToDataUseRestrictionChanges();
   }
 
   initializeForms(): void {
     this.form = this.fb.group({
       data_use_restriction: this.metadataForm.getControl('project.content.data_use_restriction'),
+      duos_id: [{value: '', disabled: true}, Validators.compose([
+        Validators.required,
+        Validators.pattern(/^DUOS-\d{6}$/)
+      ])]
     });
+  }
 
-    // Set default value for `data_use_restriction` immediately after form creation
-    let dataUseRestrictionControl = this.metadataForm.getControl('project.content.data_use_restriction');
-
-    if (!dataUseRestrictionControl) {
-      // Control doesn't exist yet, so we add it
-      this.metadataForm.formGroup.addControl('project.content.data_use_restriction', new FormControl('-'));
-      dataUseRestrictionControl = this.metadataForm.getControl('project.content.data_use_restriction');
-    } else {
-      // Control exists, we just set its initial value
+  setDefaultDataUseRestrictionValue(): void {
+    const dataUseRestrictionControl = this.form.get('data_use_restriction');
+    if (!dataUseRestrictionControl.value) {
+      // Set default value for `data_use_restriction` immediately after form creation
       dataUseRestrictionControl.setValue('null');
     }
-
-    this.duosIdCtrl = new FormControl('', Validators.compose([
-      Validators.required,
-      Validators.pattern(/^DUOS-\d{6}$/)
-    ]));
   }
 
   subscribeToDataUseRestrictionChanges(): void {
@@ -57,7 +52,7 @@ export class DataUseRestrictionGroupComponent implements OnInit {
     });
   }
 
-  showError(control: FormControl, message: string): string {
+  showError(control: AbstractControl, message: string): string {
     if (control.touched && control.errors) {
       const errors = control.errors;
 
