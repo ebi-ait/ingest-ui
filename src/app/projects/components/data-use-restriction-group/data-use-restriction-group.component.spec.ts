b40d79b21a0bb5c5ac8e1ca6059263e73eb24410
Alexandros Orges Koci <koci.orges@hotmail.com>
2024-04-11 13:14:54 +0100

    dcp-999 DUO codes & DUOS id: Added unit tests into scenario-based describe blocks

--- src/app/projects/components/data-use-restriction-group/data-use-restriction-group.component.spec.ts
+++ src/app/projects/components/data-use-restriction-group/data-use-restriction-group.component.spec.ts
@@ -1,25 +1,172 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ReactiveFormsModule, FormBuilder} from '@angular/forms';
 import { DataUseRestrictionGroupComponent } from './data-use-restriction-group.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import {MetadataForm} from '@metadata-schema-form/models/metadata-form';
 
 describe('DataUseRestrictionGroupComponent', () => {
   let component: DataUseRestrictionGroupComponent;
   let fixture: ComponentFixture<DataUseRestrictionGroupComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DataUseRestrictionGroupComponent ]
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [DataUseRestrictionGroupComponent],
+      imports: [ReactiveFormsModule],
+      providers: [FormBuilder],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
-  });
+      .compileComponents();
+  }));
 
   beforeEach(() => {
+    const schema = {
+      '$schema': 'http://json-schema.org/draft-07/schema#',
+      'additionalProperties': false,
+      'description': 'A ingest project entity contains information about the ingestion of the project.',
+      'name': 'project',
+      'properties': {
+        'identifyingOrganisms': {
+          'description': 'Organism the samples were generated from',
+          'guidelines': 'You can select multiple entries',
+          'type': 'array',
+          'items': {
+            'type': 'string'
+          },
+          'enum': [
+            'Human',
+            'Mouse',
+            'Other'
+          ],
+          'user_friendly': 'Organism the samples were generated from'
+        },
+        'content': {
+          '$id': 'https://schema.dev.data.humancellatlas.org/type/project/19.0.0/project',
+          '$schema': 'http://json-schema.org/draft-07/schema#',
+          'additionalProperties': false,
+          'description': 'A project entity contains information about the overall project.',
+          'name': 'project',
+          'type': 'object',
+          'properties': {
+            'project_core': {
+              '$schema': 'http://json-schema.org/draft-07/schema#',
+              '$id': 'http://schema.dev.data.humancellatlas.org/core/project/7.0.5/project_core',
+              'description': 'Information about the project.',
+              'additionalProperties': false,
+              'required': [
+                'project_short_name'
+              ],
+              'title': 'Project core',
+              'name': 'project_core',
+              'type': 'object',
+              'properties': {
+                'project_short_name': {
+                  'description': 'A short name for the project.',
+                  'type': 'string',
+                  'example': 'CoolOrganProject.',
+                  'user_friendly': 'Project label'
+                }
+              }
+            },
+            "data_use_restriction": {
+              "description": "Data use restrictions that apply to the project.",
+              "type": "string",
+              "enum": [
+                "NRES",
+                "GRU",
+                "GRU-NCU"
+              ],
+              "user_friendly": "Data use restriction",
+              "guidelines": "Must be one of: NRES, GRU, GRU-NCU. The use restriction codes are based on the DUO ontology where NRES corresponds to DUO:0000004, GRU corresponds to DUO:0000042, GRU-NCU corresponds to a combination of DUO:0000042 and DUO:0000046",
+              "example": "GRU"
+            },
+            "duos_id": {
+              "description": "A DUOS dataset id.",
+              "type": "string",
+              "pattern": "^DUOS-\\d{6}$",
+              "example": "DUOS-000108; DUOS-000114",
+              "user_friendly": "DUOS ID",
+              "guidelines": "Managed access projects are registered in DUOS to regulate access. If the project is managed access record the corresponding DUOS ID here."
+            }
+          }
+        }
+      },
+      'required': [],
+      'title': 'Ingest Project',
+      'type': 'object',
+      '$id': ''
+    };
+
+    const metadataForm = new MetadataForm('project', schema);
     fixture = TestBed.createComponent(DataUseRestrictionGroupComponent);
     component = fixture.componentInstance;
+    component.metadataForm = metadataForm;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('duosIdCtrl enabled/disabled based on data_use_restriction value', () => {
+
+    it('should enable duosIdCtrl when data_use_restriction is GRU', () => {
+      component.form.get('data_use_restriction').setValue('GRU');
+      expect(component.form.get('duos_id').enabled).toBeTrue();
+    });
+
+    it('should enable duosIdCtrl when data_use_restriction is GRU-NCU', () => {
+      component.form.get('data_use_restriction').setValue('GRU-NCU');
+      expect(component.form.get('duos_id').enabled).toBeTrue();
+    });
+
+    it('should disable duosIdCtrl when data_use_restriction is NRES', () => {
+      component.form.get('data_use_restriction').setValue('NRES');
+      expect(component.form.get('duos_id').disabled).toBeTrue();
+    });
+
+  });
+
+  describe('duosIdCtrl validators', () => {
+
+    let duosIdControl;
+
+    beforeEach(() => {
+      duosIdControl = component.form.get('duos_id');
+      duosIdControl.enable(); // Make sure control is enabled for validation tests
+    });
+
+    it('should be invalid with a non-matching pattern', () => {
+      duosIdControl.setValue('invalid');
+      expect(duosIdControl.valid).toBeFalse();
+    });
+
+    it('should be valid with a correctly formatted DUOS ID', () => {
+      duosIdControl.setValue('DUOS-123456');
+      expect(duosIdControl.valid).toBeTrue();
+    });
+
+  });
+
+  describe('DataUseRestrictionGroupComponent with ignoreExample', () => {
+    function initializeFormControls(ignoreExample: boolean) {
+      // Simulate initializing form controls based on ignoreExample
+      const exampleValue = ignoreExample ? null : 'GRU';
+      component.metadataForm.formGroup = new FormBuilder().group({
+        'data_use_restriction': [exampleValue]
+      });
+    }
+
+    it('should default to null when ignoreExample is true', () => {
+      initializeFormControls(true);
+      expect(component.metadataForm.formGroup.get('data_use_restriction').value).toBeNull();
+    });
+
+    it('should default to the example value when ignoreExample is false', () => {
+      initializeFormControls(false);
+      expect(component.metadataForm.formGroup.get('data_use_restriction').value).toEqual('GRU');
+    });
+
+  });
+
 });
