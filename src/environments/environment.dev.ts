41b88e9f13a2f27fca3a797b063d05d14a10c09a
Amnon Khen <amnon@ebi.ac.uk>
2024-06-03 10:55:36 +0100

    dcp-967 dc-1012 use auth token in ingest api calls

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,7 +5,7 @@ export const environment = {
   SCHEMA_API_URL: 'https://schema.humancellatlas.org',
   DSS_API_URL: 'https://dss.dev.data.humancellatlas.org',
   DOMAIN_WHITELIST: 'api.ingest.dev.archive.data.humancellatlas.org,ingest.dev.archive.data.humancellatlas.org',
-  SECURED_ENDPOINTS: '\/auth\/.*,\/user\/.*,\/submissionEnvelopes$,\/projects$',
+  SECURED_ENDPOINTS: '\/auth\/.*,\/user\/.*,\/submissionEnvelopes\/?.*,\/projects$',
   // AAI
   AAI_CLIENT_ID: 'e2041c2d-9449-4468-856e-e84711cebd21',
   AAI_AUTHORITY: 'https://login.elixir-czech.org/oidc',
