bf79682ebf4a85fdb9b214d5d732e2838b303a73
Alexandros Orges Koci <koci.orges@hotmail.com>
2024-06-11 16:55:33 +0100

    dcp-1020 fix: Removed troubleshooting log messages

--- src/app/aai/oidc-interceptor.ts
+++ src/app/aai/oidc-interceptor.ts
@@ -10,7 +10,6 @@ import {AaiService} from './aai.service';
   providedIn: AaiSecurity,
 })
 export class OidcInterceptor implements HttpInterceptor {
-
   constructor(private aai: AaiService) {
   }
 
@@ -27,44 +26,28 @@ export class OidcInterceptor implements HttpInterceptor {
 
   private isUrlSecured(url) {
     const securedUrls = environment.SECURED_ENDPOINTS.split(',');
-    console.log(`Secured URLs: ${securedUrls}`);
     const matches = securedUrls.filter(pattern => {
       const regex = new RegExp(pattern);
-      const isMatch = regex.test(url);
-      console.log(`Testing URL: ${url} against pattern: ${pattern} -> Match: ${isMatch}`);
       return regex.test(url);
     });
-    console.log(`Matches found: ${matches.length}`);
     return matches.length > 0;
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const hostName = OidcInterceptor.getHostName(request.url);
 
-    console.log(`Intercepting request to: ${request.url}`);
-    console.log(`Host name: ${hostName}`);
-    console.log('Using environment configuration:', environment);
-    console.log(`Env SECURED_ENDPOINTS: ${environment.SECURED_ENDPOINTS}`);
-    console.log(`DOMAIN_WHITELIST: ${environment.DOMAIN_WHITELIST}`);
-    console.log(`Is URL secured: ${this.isUrlSecured(request.url)}`);
-    const urlSecured = this.isUrlSecured(request.url);
-    console.log(`Is URL secured: ${urlSecured}`);
-
-    if (hostName && environment.DOMAIN_WHITELIST.indexOf(hostName) > -1 && (request.method !== 'GET' || urlSecured)) {
+    if (hostName && environment.DOMAIN_WHITELIST.indexOf(hostName) > -1 && (request.method !== 'GET' || this.isUrlSecured(request.url))) {
       return this.aai.userAuthHeader().pipe(
         concatMap(authHeader => {
-          console.log(`Retrieved Authorization header: ${authHeader}`);
           const headerRequest = request.clone({
             setHeaders: {
               Authorization: authHeader
             }
           });
-          console.log(`Adding Authorization header for: ${request.url}`);
           return next.handle(headerRequest);
         })
       );
     } else {
-      console.log(`No Authorization header added for: ${request.url}`);
       return next.handle(request);
     }
   }
